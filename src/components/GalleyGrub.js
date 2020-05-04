09244133c9e57e8c329036c7074e1861a7aec65e
César Rivas <61394656+csarrvas@users.noreply.github.com>
2020-05-04 11:26:14 -0600

    Funcionalidad de persistir data
    Se ha agregado la función de persistir los datos en un archivo .json por medio de json-server

--- src/components/GalleyGrub.js
+++ src/components/GalleyGrub.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './css/galleyGrub.css';
+import clientAxios from '../config/axios';
+import { printMessage } from '../helpers/ui';
 
-const sendToJsonFile = (data) => {
-  // console.log(data);
-  // this function has to send the 'data' to a .json file
+const updateJsonFile = async (data) => {
+  try {
+    const response = await clientAxios.get('/hamburgers');
+    response.data.forEach(hamburger => {
+      clientAxios.delete(`/hamburgers/${hamburger.id}`);
+    });
+    
+    data.forEach(hamburger => {
+      clientAxios.post('/hamburgers', hamburger);
+    });
+
+    printMessage('Galley Grub Updated with your hamburgers', 'success', '#upload');
+
+  } catch(error) {
+    printMessage('You have to run json-server db.json --port 4000', 'error', '#upload');
+  }
 }
 
 const GalleyGrub = ({ ls }) => {
-  const data = JSON.stringify(ls.getAllBurgers());
+  const data = ls.getAllBurgers();
   return (
     <div id="upload">
-      <button onClick={() => sendToJsonFile(data)}>Upload to Galley Grub</button>
+      <button onClick={() => updateJsonFile(data)}>Upload to Galley Grub</button>
     </div>
   );
 }
