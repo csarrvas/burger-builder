80a0f4602e2442a14a0e2e42fa6f4ad6a9017429
César Rivas <61394656+csarrvas@users.noreply.github.com>
2020-05-05 15:41:01 -0600

    Update Burger.js
    Component optimization

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -8,32 +8,42 @@ import PropTypes from 'prop-types';
 class Burger extends Component {
   constructor(props) {
     super(props);
-    this.state = this.props.burger ?  { ingredients: this.props.burger.ingredients } : { ingredients: [] };
+    // this.state = this.props.burger ?  { ingredients: this.props.burger.ingredients } : { ingredients: [] };
+    this.state = {
+      ingredients: this.props.burger ? this.props.burger.ingredients : []
+    };
   }
 
-  addIngredient(e) {
+  addIngredient = (e) => {
     const newIngredient = e.target.value;
 
     if (availableIngredients.indexOf(newIngredient) !== -1) {
-      this.setState(state => {
-        const ingredients = [newIngredient, ...state.ingredients];
-        return { ingredients };
+      // this.setState((state, prevState) => {
+      //   const ingredients = [newIngredient, ...state.ingredients];
+      //   return { ingredients };
+      // });
+      this.setState({
+        ingredients: [newIngredient, ...this.state.ingredients]
       });
     }
   }
 
-  removeIngredient(e) {
+  removeIngredient = (e) => {
+    const {ingredients} = this.state;
     const indexToRemove = this.state.ingredients.indexOf(e.target.value);
 
     if (indexToRemove !== -1) {
-      this.setState(state => {
-        const ingredients = state.ingredients.filter((ingredient, index) => index !== indexToRemove);
-        return { ingredients };
-      });
+      // this.setState((state, prevState) => {
+      //   const ingredients = state.ingredients.filter((ingredient, index) => index !== indexToRemove);
+      //   return { ingredients };
+      // });
+      this.setState({
+        ingredients: ingredients.filter((ingredient, index) => index !== indexToRemove)
+      })
     }
   }
 
-  saveBurger() {
+  saveBurger = () => {
     const inputName = document.getElementById('burgerName');
     if (inputName.value) {
       const newBurger = {
@@ -60,6 +70,8 @@ class Burger extends Component {
     }
   }
 
+  resetState = () => this.setState({ ingredients: [] });
+
   render() {
     return (
       <div id="burger-builder">
@@ -71,19 +83,19 @@ class Burger extends Component {
         <Details ingredients={this.state.ingredients} />
         <p>Tell SpongeBob your ingredients from bottom to top!</p>
         <div id="add-buttons">
-          {availableIngredients.map(ingredient => <button key={`Add ${ingredient}`} className="add-ingredient" value={ingredient} onClick={e => this.addIngredient(e)}>{`${ingredient} +1`}</button>)}
+          {availableIngredients.map(ingredient => <button key={`Add ${ingredient}`} className="add-ingredient" value={ingredient} onClick={this.addIngredient}>{`${ingredient} +1`}</button>)}
         </div>
         {this.state.ingredients.length ? <p>Tell SpongeBob a ingredient to remove</p> : null}
         <div id="remove-buttons">
-          {availableIngredients.map(ingredient => this.state.ingredients.includes(ingredient) ? <button key={`Remove ${ingredient}`} className="remove-ingredient" value={ingredient} onClick={e => this.removeIngredient(e)}>{`${ingredient} -1`}</button> :  null)}
+          {availableIngredients.map(ingredient => this.state.ingredients.includes(ingredient) ? <button key={`Remove ${ingredient}`} className="remove-ingredient" value={ingredient} onClick={this.removeIngredient}>{`${ingredient} -1`}</button> :  null)}
         </div>
         <figure id="burger-ingredients">
           <img alt="burger ingredients" src="images/krabby-patty.png"/>
         </figure>
         <form onSubmit={e => e.preventDefault()}>
           <input type="text" id="burgerName" placeholder="Krabby Patty Name" defaultValue={this.props.burger ? this.props.burger.name : ''}/>
-          <button id="save_button" onClick={() => this.saveBurger()}>{`${this.props.burger ? 'Update' : 'Save'} Burger`}</button>
-          <button id="reset_button" onClick={() => this.setState({ ingredients: [] })}>Reset Burger</button>
+          <button id="save_button" onClick={this.saveBurger}>{`${this.props.burger ? 'Update' : 'Save'} Burger`}</button>
+          <button id="reset_button" onClick={this.resetState}>Reset Burger</button>
         </form>
       </div>
     );
